ea417886d8900a65a12756e799877c0d614ef1ec
PointSource <47544779+point-source@users.noreply.github.com>
2020-09-18 19:51:24 -0700

    Implement basic snmp get, getNext, walk operations

--- lib/src/snmp_base.dart
+++ lib/src/snmp_base.dart
@@ -1,5 +1,6 @@
 // TODO: Put public facing types in this file.
 
+import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 
@@ -7,6 +8,7 @@ import 'package:dart_snmp/src/models/authentication.dart';
 import 'package:dart_snmp/src/models/message.dart';
 import 'package:dart_snmp/src/models/oid.dart';
 import 'package:dart_snmp/src/models/pdu.dart';
+import 'package:dart_snmp/src/models/request.dart';
 import 'package:dart_snmp/src/models/varbind.dart';
 
 class Snmp {
@@ -50,6 +52,7 @@ class Snmp {
   Duration timeout = Duration(seconds: 5);
   SnmpVersion version = SnmpVersion.V2c;
   RawDatagramSocket socket;
+  Map<int, Request> requests = {};
 
   Future<void> _bind(InternetAddress address, int port) async {
     socket = await RawDatagramSocket.bind(address, port);
@@ -66,13 +69,16 @@ class Snmp {
     if (d == null) return;
 
     var msg = Message.fromBytes(d.data);
-    print(
-        'Datagram from ${d.address.address}:${d.port}: ${msg.pdu.varbinds[0].value}');
+    if (requests.containsKey(msg.pdu.requestId)) {
+      requests[msg.pdu.requestId].complete(msg);
+    }
+/*     print(
+        'Datagram from ${d.address.address}:${d.port}: ${msg.pdu.varbinds[0].value}'); */
   }
 
   void _onClose() {
     // TODO(andrew): Handle closing
-    _cancelRequests(Exception('Socket forcibly closed'));
+    _cancelRequests(Exception('Socket forcibly closed. HANDLE ME'));
   }
 
   void _onError(Object error) {
@@ -86,15 +92,45 @@ class Snmp {
     throw error;
   }
 
+  void _cancelRequest(int requestId) => requests.remove(requestId);
+
   int _generateId(int bitSize) => bitSize == 16
       ? (Random().nextInt(10000) % 65535).floor()
       : (Random().nextInt(100000000) % 4294967295).floor();
 
-  void get(List<Oid> oids) {}
+  Future<Message> get(Oid oid) => _get(oid, PduType.GetRequest);
 
-  int send(List<Varbind> v) {
-    var pdu = Pdu(PduType.GetRequest, 1, v);
-    var msg = Message(version, community, pdu);
-    return socket.send(msg.encodedBytes, target, port);
+  Future<Message> getNext(Oid oid) => _get(oid, PduType.GetNextRequest);
+
+  Stream<Message> walk() async* {
+    var oid = Oid.fromString('1.3');
+    while (true) {
+      var msg = await getNext(oid);
+      oid = msg.pdu.varbinds.last.oid;
+      if (msg.pdu.error == PduError.NoSuchName) {
+        close();
+      } else {
+        yield msg;
+      }
+    }
+  }
+
+  Future<Message> _get(Oid oid, PduType type) {
+    var c = Completer<Message>();
+    var v = Varbind<String>(oid, VarbindType.Null, null);
+    var p = Pdu(type, _generateId(16), [v]);
+    while (requests.containsKey(p.requestId)) {
+      p.requestId = _generateId(16);
+    }
+    var m = Message(version, community, p);
+    var r = Request(target, port, m, timeout, retries, c.complete,
+        c.completeError, _cancelRequest);
+    _send(r);
+    return c.future;
+  }
+
+  void _send(Request r) {
+    socket.send(r.message.encodedBytes, r.target, r.port);
+    requests[r.requestId] = r;
   }
 }
